1f2ece8987e1e80244dc7411b39ec5d0edd572cf
ishatr <isha212195@gmail.com>
2017-03-24 13:34:45 +0530

    minor changes to form :zap:

--- app/components/QueryForm.js
+++ app/components/QueryForm.js
@@ -37,13 +37,35 @@ export default class QueryForm extends Component {
         return (
             <div className="form-container content-wrapper">
                 <input className="form__input" placeholder="System Id" type="text" name="student" value={this.state.student} onChange={this.handleChange} />
+                <br />
                 <input className="form__input" placeholder="Phone Number" type="text" name="phone" value={this.state.phone} onChange={this.handleChange} />
+                <br />
                 <input className="form__input" placeholder="Email" type="text" name="email" value={this.state.email} onChange={this.handleChange} />
-                <input className="form__input" placeholder="Query Type" type="text" name="query_type" value={this.state.query_type} onChange={this.handleChange} />
-                <input className="form__input" placeholder="Department" type="text" name="department" value={this.state.department} onChange={this.handleChange} />
-                <input className="form__input" placeholder="Year" type="text" name="year" value={this.state.year} onChange={this.handleChange} />
-                <input className="form__input" placeholder="Status" type="text" name="status" value={this.state.status} onChange={this.handleChange} />
+                <br />
+                <select className="form__input" placeholder="Query Type" type="text" name="query_type" value={this.state.query_type} onChange={this.handleChange}>
+                    <option>Result Discrepancy</option>
+                    <option>Credit Discrepancy</option>
+                    <option>PDC Issue</option>
+                    <option>Other Document Issue</option>
+                </select>
+                <br />
+                <select className="form__input" placeholder="Department" type="text" name="department" value={this.state.department} onChange={this.handleChange}>
+                    <option>CSE</option>
+                    <option>ME</option>
+                    <option>CE</option>
+                    <option>EEE</option>
+                    <option>Biotech</option>
+                </select>
+                <br />
+                <select className="form__input" placeholder="Year" type="text" name="year" value={this.state.year} onChange={this.handleChange}>
+                    <option>1st</option>
+                    <option>2nd</option>
+                    <option>3rd</option>
+                    <option>4th</option>
+                </select>
+                <br />
                 <input className="form__input" placeholder="Description" type="textarea" name="description" value={this.state.description} onChange={this.handleChange} />
+                <br />
                 <button onClick={this.onSubmit} className="button button-next">Save</button>
             </div>
             
